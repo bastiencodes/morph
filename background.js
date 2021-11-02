1751a36110562cd25473fcd3eb53c5e95475515a
Bastien <bastienm95@gmail.com>
2021-11-02 23:18:01 +0100

    Log all tabs to console

--- background.js
+++ background.js
@@ -3,6 +3,10 @@ chrome.runtime.onInstalled.addListener((details) => {
   console.log("Details", details);
 });
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
   console.log(tab);
+
+  const { windowId } = tab;
+  const tabs = await chrome.tabs.query({ windowId });
+  console.log(tabs);
 });
