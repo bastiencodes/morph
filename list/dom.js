d85c8e62de8423a671d055fd886140247e6b7b47
Bastien <bastienm95@gmail.com>
2021-11-25 15:27:33 +0100

    Add anchor tag to each item in list view

--- list/dom.js
+++ list/dom.js
@@ -1,5 +1,15 @@
+function createLink(tab) {
+  const a = document.createElement("a");
+  a.innerText = tab.title;
+  a.setAttribute("href", tab.url);
+  a.setAttribute("target", "_blank");
+  a.setAttribute("rel", "noopener");
+  return a;
+}
+
 function createListItem(tab) {
-  const item = document.createElement("li");
-  item.innerText = `${tab.title} | ${tab.url}`;
-  return item;
+  const li = document.createElement("li");
+  const link = createLink(tab);
+  li.appendChild(link);
+  return li;
 }
