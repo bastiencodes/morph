661eab9e76cde74a2f9aa8844b1b8fe45f2daba6
Bastien <bastienm95@gmail.com>
2021-11-29 18:53:00 +0100

    Create context menu

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
+import { createContextMenu } from "./helpers/menu.js";
 import { sendAll } from "./helpers/tabs.js";
 
 chrome.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed!");
   console.log("Details", details);
+  createContextMenu();
 });
 
 chrome.action.onClicked.addListener(async (currentTab) => {
---
+++ helpers/menu.js
@@ -0,0 +1,34 @@
+const TITLE_DISPLAY = "Display Morph";
+const TITLE_SEND_ALL = "Send all tabs to Morph";
+const TITLE_SEND_ONLY = "Send only this tab to Morph";
+const TITLE_SEND_EXCEPT = "Send all tabs except this tab to Morph";
+const TITLE_SEND_LEFT = "Send tabs on the left to Morph";
+const TITLE_SEND_RIGHT = "Send tabs on the right to Morph";
+const TITLE_SEND_ALL_WINDOWS = "Send all tabs from all windows to Morph";
+const TITLE_EXCLUDE = "Exclude host from Morph";
+const TITLE_HELP = "Help";
+
+const ID_DISPLAY = "display";
+const ID_SEND_ALL = "send_all";
+const ID_SEND_ONLY = "send_only";
+const ID_SEND_EXCEPT = "send_except";
+const ID_SEND_LEFT = "send_left";
+const ID_SEND_RIGHT = "send_right";
+const ID_SEND_ALL_WINDOWS = "send_all_windows";
+const ID_EXCLUDE = "exclude";
+const ID_HELP = "help";
+
+export function createContextMenu() {
+  chrome.contextMenus.create({ id: ID_DISPLAY, title: TITLE_DISPLAY });
+  chrome.contextMenus.create({ id: ID_SEND_ALL, title: TITLE_SEND_ALL });
+  chrome.contextMenus.create({ id: ID_SEND_ONLY, title: TITLE_SEND_ONLY });
+  chrome.contextMenus.create({ id: ID_SEND_EXCEPT, title: TITLE_SEND_EXCEPT });
+  chrome.contextMenus.create({ id: ID_SEND_LEFT, title: TITLE_SEND_LEFT });
+  chrome.contextMenus.create({ id: ID_SEND_RIGHT, title: TITLE_SEND_RIGHT });
+  chrome.contextMenus.create({
+    id: ID_SEND_ALL_WINDOWS,
+    title: TITLE_SEND_ALL_WINDOWS,
+  });
+  chrome.contextMenus.create({ id: ID_EXCLUDE, title: TITLE_EXCLUDE });
+  chrome.contextMenus.create({ id: ID_HELP, title: TITLE_HELP });
+}
