f7f7f6a82f4921062b53578f04f6d91b9e5a1216
Bastien <bastienm95@gmail.com>
2021-11-29 19:13:59 +0100

    Add createMenuItem and createSeparator functions

--- helpers/menu.js
+++ helpers/menu.js
@@ -22,23 +22,28 @@ import {
   TITLE_HELP,
 } from "./constants.js";
 
+function createMenuItem(id, title) {
+  chrome.contextMenus.create({ id, title });
+}
+
+function createSeparator(id) {
+  chrome.contextMenus.create({ id, type: "separator" });
+}
+
 export function createContextMenu() {
-  chrome.contextMenus.create({ id: ID_DISPLAY, title: TITLE_DISPLAY });
-  chrome.contextMenus.create({ id: ID_SEND_ALL, title: TITLE_SEND_ALL });
-  chrome.contextMenus.create({ id: ID_SEPARATOR_1, type: "separator" });
+  createMenuItem(ID_DISPLAY, TITLE_DISPLAY);
+  createMenuItem(ID_SEND_ALL, TITLE_SEND_ALL);
+  createSeparator(ID_SEPARATOR_1);
 
-  chrome.contextMenus.create({ id: ID_SEND_ONLY, title: TITLE_SEND_ONLY });
-  chrome.contextMenus.create({ id: ID_SEND_EXCEPT, title: TITLE_SEND_EXCEPT });
-  chrome.contextMenus.create({ id: ID_SEND_LEFT, title: TITLE_SEND_LEFT });
-  chrome.contextMenus.create({ id: ID_SEND_RIGHT, title: TITLE_SEND_RIGHT });
-  chrome.contextMenus.create({
-    id: ID_SEND_ALL_WINDOWS,
-    title: TITLE_SEND_ALL_WINDOWS,
-  });
-  chrome.contextMenus.create({ id: ID_SEPARATOR_2, type: "separator" });
+  createMenuItem(ID_SEND_ONLY, TITLE_SEND_ONLY);
+  createMenuItem(ID_SEND_EXCEPT, TITLE_SEND_EXCEPT);
+  createMenuItem(ID_SEND_LEFT, TITLE_SEND_LEFT);
+  createMenuItem(ID_SEND_RIGHT, TITLE_SEND_RIGHT);
+  createMenuItem(ID_SEND_ALL_WINDOWS, TITLE_SEND_ALL_WINDOWS);
+  createSeparator(ID_SEPARATOR_2);
 
-  chrome.contextMenus.create({ id: ID_EXCLUDE, title: TITLE_EXCLUDE });
-  chrome.contextMenus.create({ id: ID_SEPARATOR_3, type: "separator" });
+  createMenuItem(ID_EXCLUDE, TITLE_EXCLUDE);
+  createSeparator(ID_SEPARATOR_3);
 
-  chrome.contextMenus.create({ id: ID_HELP, title: TITLE_HELP });
+  createMenuItem(ID_HELP, TITLE_HELP);
 }
