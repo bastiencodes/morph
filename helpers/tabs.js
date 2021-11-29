25ed68b18dbd2db3007ae642d116147b38f49536
Bastien <bastienm95@gmail.com>
2021-11-29 20:56:29 +0100

    Add helpers to determine tab position

--- helpers/tabs.js
+++ helpers/tabs.js
@@ -61,3 +61,25 @@ export async function sendAll(currentTab) {
   const tabs = await getAllTabsInWindow(currentTab);
   return sendTabs(tabs);
 }
+
+// helpers to determine tab position
+function findTab(tabId, tabs) {
+  const tab = tabs.find((tab) => tab.id === tabId);
+  if (!tab) throw new Error("No tab found");
+  return tab;
+}
+
+export function isTabMostLeft(tabId, tabs) {
+  const tab = findTab(tabId, tabs);
+  return tab.index === 0;
+}
+
+export function isTabMostRight(tabId, tabs) {
+  const tab = findTab(tabId, tabs);
+  return tab.index === tabs.length - 1;
+}
+
+export function isOnlyTabInWindow(tabId, tabs) {
+  const tab = findTab(tabId, tabs);
+  return tab && tabs.length === 1;
+}
