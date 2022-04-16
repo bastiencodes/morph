d2bdf314cb479df102fb07162cdaec59711d3af6
Bastien <bastienm95@gmail.com>
2022-04-16 15:42:58 +0200

    Move pages to root of pages dir

--- constants/paths.js
+++ constants/paths.js
@@ -1,5 +1,5 @@
-export const LIST_VIEW_PATH = "pages/list/index.html";
-export const TRANSFER_PATH = "pages/transfer/transfer.html";
+export const LIST_VIEW_PATH = "pages/list.html";
+export const TRANSFER_PATH = "pages/transfer.html";
 
 export const ABOUT_URL = "https://morph.github.io";
 export const HELP_URL = "https://morph.github.io/help";
