0d97d07b950c26db7e93c0795cf767a2675eb9db
Bastien <bastienm95@gmail.com>
2022-04-19 00:20:40 +0200

    Refactor checkOptions

--- helpers/options.js
+++ helpers/options.js
@@ -1,39 +1,19 @@
-import { getTabGroups } from "./storage.js";
+import { uniqueBy } from "./array.js";
+import { getTabURLs } from "./storage.js";
 
-function removePinnedTabs(tabs) {
-  return tabs.filter((tab) => !tab.pinned);
-}
-
-// tabs with same URLs
-async function removeDuplicateTabs(tabs) {
-  const tabGroups = await getTabGroups();
-  const storedTabs = Object.values(tabGroups)
-    .map((tabGroup) => tabGroup.tabs)
-    .flat(1);
-  const storedTabURLs = storedTabs.map((tab) => tab.url);
-
-  const urls = [...storedTabURLs];
-  const tabsWithNoDuplicates = tabs.filter((tab) => {
-    const { url } = tab;
-    const isUnique = !urls.includes(url);
-    urls.push(url);
-    return isUnique;
-  });
-
-  return tabsWithNoDuplicates;
-}
+const isUnpinned = (tab) => !tab.pinned;
 
 export async function checkOptions(tabs, options) {
-  let updatedTabs = tabs;
-
+  let updatedTabs = [...tabs];
   const { ALLOW_PINNED_TABS, ALLOW_DUPLICATES } = options;
 
   if (!ALLOW_PINNED_TABS) {
-    updatedTabs = removePinnedTabs(updatedTabs);
+    updatedTabs = updatedTabs.filter(isUnpinned);
   }
 
   if (!ALLOW_DUPLICATES) {
-    updatedTabs = await removeDuplicateTabs(updatedTabs);
+    const storedTabURLs = await getTabURLs();
+    updatedTabs = uniqueBy(updatedTabs, "url", storedTabURLs);
   }
 
   return updatedTabs;
