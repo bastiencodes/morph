c47ea065fdbef3930dd11ac3ff51f0060dd9b535
Bastien <bastienm95@gmail.com>
2022-04-18 18:01:33 +0200

    Add isListPageURL helper

--- helpers/urls.js
+++ helpers/urls.js
@@ -2,4 +2,6 @@ import { LIST_VIEW_PATH, TRANSFER_PATH } from "../constants/paths";
 
 export const getListPageURL = () => chrome.runtime.getURL(LIST_VIEW_PATH);
 
+export const isListPageURL = (url) => url === getListPageURL();
+
 export const getTransferPageURL = () => chrome.runtime.getURL(TRANSFER_PATH);
